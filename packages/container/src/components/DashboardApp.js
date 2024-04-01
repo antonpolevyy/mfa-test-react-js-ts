9ddefff6543617dfcac732cfe0b41ead925a8805
Anton P <polevyy.anton@gmail.com>
2024-04-01 22:34:20 +1100

    Failed attempt to import Dashboard

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,12 +1,34 @@
-import { mount } from 'dashboard/DashboardApp';
-import React, { useRef, useEffect } from 'react';
+// import { mount } from 'dashboard/DashboardApp';
+// import React, { useRef, useEffect } from 'react';
+
+// export default () => {
+//   const ref = useRef(null);
+
+//   useEffect(() => {
+//     mount(ref.current);
+//   }, []);
+
+//   return <div ref={ref} />;
+// };
+
+import React from 'react';
 
 export default () => {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    mount(ref.current);
-  }, []);
-
-  return <div ref={ref} />;
+  console.log('DashboardApp()');
+  return <>DASHBOARD APP</>;
 };
+
+// import React from 'react';
+// import UserProfile from 'dashboard/UserProfile';
+
+// const USER_DATA = {
+//   id: 'gfhjk56rjkh6578',
+//   name: 'say my name',
+//   avatarURL:
+//     'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg',
+// };
+
+// export default () => {
+//   console.log('DashboardApp()');
+//   return <UserProfile userData={USER_DATA} />;
+// };
--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+import packageJson from './package.json';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './UserProfile': './src/components/UserProfile',
+      },
+      // shared: ['react', 'react-dom'],
+      shared: packageJson.dependencies,
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
